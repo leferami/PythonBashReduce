ff5f861d60fd63f7c9aa44c9ccf91037133dc8b1
hjupiter <hector_franklin_17@yahoo.es>
2016-01-28 14:13:58 -0500

    actualizacion script

--- srStem.py
+++ srStem.py
@@ -8,9 +8,14 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 
 ps = PorterStemmer()
+worktime = 0
+startTime = time.time()
 for line in sys.stdin:
 	if(len(line)!=1):
 		words = word_tokenize(line)
 		for w in words:
-			print(ps.stem(w))
-			
+			#print(ps.stem(w))
+			ps.stem(w)
+		endTime = time.time()
+		worktime = endTime - startTime
+		print worktime
